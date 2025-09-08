32d33f21fc5497e066b4aa5aa2578af2bd788260
Tevis Lucas <lubaking.co@gmail.com>
2025-09-08 21:49:41 +0100

    sept 2025 updates

--- src/frontend/FooterCopyright.jsx
+++ src/frontend/FooterCopyright.jsx
@@ -11,7 +11,38 @@ const FooterCopyright = () => {
           className="image-12"
         />
       </div>
-      <p className="text-small footer-bottom-paragraph paragraph-2">©2025 All Rights Reserved | EOSI Finance</p>
+      <p className="text-small footer-bottom-paragraph paragraph-2">©2025 All Rights Reserved | EOSI Finance |
+       
+             <a
+              href="https://eosi-finance-1.gitbook.io/eosi-finance-documentations/eosi-finance-general-legal-framework/terms-of-service-eosi-finance"
+              className="link-block w-inline-block"
+              target="_blank" // Opens the link in a new tab
+              rel="noopener noreferrer" // Security feature to prevent potential vulnerabilities
+            >
+              <p className="text-small">Terms of Service |</p>
+            </a>
+
+
+           
+             <a
+              href="https://eosi-finance-1.gitbook.io/eosi-finance-documentations/eosi-finance-general-legal-framework/privacy-policy-eosi-finance"
+              className="link-block w-inline-block"
+              target="_blank" // Opens the link in a new tab
+              rel="noopener noreferrer" // Security feature to prevent potential vulnerabilities
+            >
+              <p className="text-small">Privacy Policy |</p>
+            </a>
+
+            
+             <a
+              href="https://eosi-finance-1.gitbook.io/eosi-finance-documentations/eosi-finance-general-legal-framework/disclaimer-eosi-finance"
+              className="link-block w-inline-block"
+              target="_blank" // Opens the link in a new tab
+              rel="noopener noreferrer" // Security feature to prevent potential vulnerabilities
+            >
+              <p className="text-small">Disclaimer</p>
+            </a>
+      </p>
     </div>
   );
 };
